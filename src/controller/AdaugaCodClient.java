535218b67f0765f19e721952f0d120636831c017
valymirauta <mirautav@gmail.com>
2020-11-07 21:13:05 +0200

    s-a corectat metoda de adaugare facturi

--- src/controller/AdaugaCodClient.java
+++ src/controller/AdaugaCodClient.java
@@ -3,16 +3,15 @@ package controller;
 import database.DatabaseHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
-import main.Main;
 
 import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class AdaugaCodClient implements Initializable {
@@ -51,59 +50,89 @@ public class AdaugaCodClient implements Initializable {
     void adaugaCodClient(ActionEvent event) {
 
         String codClient = txtCodClient.getText();
-        String tipUtilitate = txtTipUtilitate.getText();
+       if( verificaCodClient()){
+           Alert alert = new Alert(Alert.AlertType.WARNING);
+           alert.setHeaderText("Codul de client a fost deja introdus");
+           alert.setContentText("Introduceti alt cod.");
+           alert.showAndWait();
 
-        if (codClient.isEmpty() || tipUtilitate.isEmpty()) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setHeaderText(null);
-            alert.setContentText("Fill required fields!");
-            alert.showAndWait();
-        } else {
-            String selected = alegeAsociatia.getValue();
-            System.out.println("1: "+selected);
+       } else {
+           String tipUtilitate = txtTipUtilitate.getText();
 
-            String query1 = "SELECT Id FROM asociatii WHERE denumireScurta=?";
+           if (codClient.isEmpty() || tipUtilitate.isEmpty()) {
+               Alert alert = new Alert(Alert.AlertType.ERROR);
+               alert.setHeaderText(null);
+               alert.setContentText("Fill required fields!");
+               alert.showAndWait();
+           } else {
+               String selected = alegeAsociatia.getValue();
 
-            try {
-                PreparedStatement pstmt = DatabaseHandler.conn.prepareStatement(query1);
-                pstmt.setString(1, selected);
-                ResultSet rs = pstmt.executeQuery();
-                while (rs.next()) {
-                    IdAsociatie = rs.getInt("Id");
-                    System.out.println("2: "+IdAsociatie);
+               String query1 = "SELECT Id FROM asociatii WHERE denumireScurta=?";
+
+               try {
+                   PreparedStatement pstmt = DatabaseHandler.conn.prepareStatement(query1);
+                   pstmt.setString(1, selected);
+                   ResultSet rs = pstmt.executeQuery();
+                   while (rs.next()) {
+                       IdAsociatie = rs.getInt("Id");
+                       System.out.println("2: " + IdAsociatie);
+                   }
+                   String query3 = "SELECT Id FROM asociatii_furnizori WHERE (asociatiiID=? and furnizoriID=?)";
+                   PreparedStatement pstmt3 = DatabaseHandler.conn.prepareStatement(query3);
+                   System.out.println("3: " + IdFurnizor);
+                   pstmt3.setInt(1, IdAsociatie);
+                   pstmt3.setInt(2, IdFurnizor);
+                   ResultSet rs3 = pstmt3.executeQuery();
+                   while (rs3.next()) {
+                       IdAsociatieFurnizor = rs3.getInt("Id");
+                       System.out.println("4: " + IdAsociatieFurnizor);
+                   }
+
+                   String query2 = "INSERT INTO codClient (codClient,tipUtilitate, asociatii_furnizoriID) VALUES (?,?,?)";
+                   PreparedStatement pstmt1 = DatabaseHandler.conn.prepareStatement(query2);
+                   pstmt1.setString(1, codClient);
+                   pstmt1.setString(2, tipUtilitate);
+                   pstmt1.setInt(3, IdAsociatieFurnizor);
+                   pstmt1.executeUpdate();
+
+                   Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                   alert.setHeaderText(null);
+                   alert.setContentText("Successfully added");
+                   alert.showAndWait();
+
+               } catch (SQLException ex) {
+                   Alert alert = new Alert(Alert.AlertType.ERROR);
+                   alert.setHeaderText(null);
+                   alert.setContentText("Operation failed");
+                   alert.showAndWait();
+                   System.out.println(ex);
+               }
+           }
+       }
+
+        adaugaCodClient.getScene().
+
+                getWindow().
+
+                hide();
+
+    }
+
+    public Boolean verificaCodClient() {
+        String sql = "SELECT codClient FROM codClient";
+        try {
+            Statement stmt = DatabaseHandler.conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                String codClientExistent = rs.getString("codClient");
+                if (txtCodClient.getText().equals(codClientExistent)) {
+                    return true;
                 }
-                String query3 = "SELECT Id FROM asociatii_furnizori WHERE (asociatiiID=? and furnizoriID=?)";
-                PreparedStatement pstmt3 = DatabaseHandler.conn.prepareStatement(query3);
-                System.out.println("3: "+IdFurnizor);
-                pstmt3.setInt(1, IdAsociatie);
-                pstmt3.setInt(2, IdFurnizor);
-                ResultSet rs3 = pstmt3.executeQuery();
-                while (rs3.next()) {
-                    IdAsociatieFurnizor = rs3.getInt("Id");
-                    System.out.println("4: "+IdAsociatieFurnizor);
-                }
-
-                String query2 = "INSERT INTO codClient (codClient,tipUtilitate, asociatii_furnizoriID) VALUES (?,?,?)";
-                PreparedStatement pstmt1 = DatabaseHandler.conn.prepareStatement(query2);
-                pstmt1.setString(1, codClient);
-                pstmt1.setString(2, tipUtilitate);
-                pstmt1.setInt(3, IdAsociatieFurnizor);
-                pstmt1.executeUpdate();
-
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setHeaderText(null);
-                alert.setContentText("Successfully added");
-                alert.showAndWait();
-
-            } catch (SQLException ex) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setHeaderText(null);
-                alert.setContentText("Operation failed");
-                alert.showAndWait();
-                System.out.println(ex);
             }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
-        adaugaCodClient.getScene().getWindow(). hide();
+        return  false;
     }
 
     @Override
@@ -130,5 +159,13 @@ public class AdaugaCodClient implements Initializable {
     }
 
 }
+
+
+//cautare cod client pentru furnzior Aquatim
+
+//SELECT codClient, denumire  FROM codClient
+// LEFT JOIN asociatii_furnizori ON codClient.asociatii_furnizoriID=asociatii_furnizori.Id
+// LEFT JOIN furnizori ON asociatii_furnizori.furnizoriID=furnizori.Id
+// WHERE denumire LIKE 'Aquatim%'
 
 
